4a518ae441eecb95070383839b90f87e1cede10d
noran-seliem <noran.aslan99@eng-st.cu.edu.eg>
2021-10-06 22:09:36 +0200

    profile-place

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,35 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+
 describe('AppComponent', () => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { appRoutingModule } from './app-routing.module';
+import { PlacePageComponent } from './place-page/place-page.component';
+
+import { NgImageSliderModule } from 'ng-image-slider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    PlacePageComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, appRoutingModule , NgImageSliderModule ,BrowserAnimationsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
